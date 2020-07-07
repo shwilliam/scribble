cdeac3aec0cb3177b33647eea4676f2237aeeacc
William Lindvall <shwilliam@hey.com>
2020-07-07 16:09:35 -0700

    add image download link

--- web/src/components/Post/Post.js
+++ web/src/components/Post/Post.js
@@ -3,8 +3,21 @@ import TimeTag from 'src/components/TimeTag'
 const Post = ({post}) => {
   return (
     <article className="rw-segment">
+      <header>
+        <TimeTag datetime={post.createdAt} />
+      </header>
+
       <img src={post.image} aria-label="TODO" />
-      <TimeTag datetime={post.createdAt} />
+
+      <footer>
+        <a
+          href={post.image}
+          download={`doodle_${post.id}.png`}
+          className="rw-button rw-button-blue"
+        >
+          Download
+        </a>
+      </footer>
     </article>
   )
 }
