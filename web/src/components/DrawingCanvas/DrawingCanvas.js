9e9136c318d12e269b1cbd5befa179db7e2a1e0d
William Lindvall <shwilliam@hey.com>
2020-07-14 14:47:10 -0700

    fix drawing canvas sizing

--- web/src/components/DrawingCanvas/DrawingCanvas.js
+++ web/src/components/DrawingCanvas/DrawingCanvas.js
@@ -2,10 +2,12 @@ import {useCssVar} from '@shwilliam/react-use-css-var'
 import {useEffect, useRef, useState} from 'react'
 
 import {useWindowSize} from 'src/hooks/use-window-size'
+import {getPixelValueFromCssString} from 'src/lib/css'
 import {getEventPosition} from 'src/lib/native-events'
 
 const DrawingCanvas = ({onDraw = () => {}, defaultValue = ''}) => {
   const [getMaxCanvasSize] = useCssVar('--drawing-canvas-max-width')
+  const [getLargeSpace] = useCssVar('--space-x-large') // horizontal margins
   const windowSize = useWindowSize()
   const canvasRef = useRef()
   const contextRef = useRef()
@@ -46,21 +48,26 @@ const DrawingCanvas = ({onDraw = () => {}, defaultValue = ''}) => {
     if (!windowSize) return
 
     const canvas = canvasRef.current
+    const singleCanvasMargin = getLargeSpace()
+    const totalCanvasMargins =
+      getPixelValueFromCssString(singleCanvasMargin, canvas) * 2
     const maxCanvasSize = getMaxCanvasSize()
-    const maxCanvasSizePixels = Number(maxCanvasSize.split('px')[0])
-    const widthScale = 0.8
-    const scaledWidth = windowSize?.width * widthScale
-    const heightScale = 0.7
-    const scaledHeight = scaledWidth * widthScale
+    const maxCanvasSizePixels = getPixelValueFromCssString(
+      maxCanvasSize,
+      canvas,
+    )
+    const scaledWidth = windowSize?.width - totalCanvasMargins
     const canvasSize =
       scaledWidth > maxCanvasSizePixels ? maxCanvasSizePixels : scaledWidth
+    const heightScale = 0.7
+    const scaledHeight = canvasSize * heightScale
 
     canvas.style.width = `${canvasSize}px`
-    canvas.style.height = `${canvasSize * heightScale}px`
+    canvas.style.height = `${scaledHeight}px`
     canvas.style.touchAction = 'none'
     // support high dpi displays
     canvas.width = canvasSize * 2
-    canvas.height = canvasSize * 2 * heightScale
+    canvas.height = 2 * scaledHeight
 
     const context = canvas.getContext('2d')
     context.lineCap = 'round'
@@ -71,12 +78,12 @@ const DrawingCanvas = ({onDraw = () => {}, defaultValue = ''}) => {
       // paint default value
       const image = new Image()
       image.onload = () =>
-        context.drawImage(image, 0, 0, scaledWidth, scaledHeight)
       image.src = defaultValue
+        context.drawImage(image, 0, 0, canvasSize, scaledHeight)
     }
 
     contextRef.current = context
-  }, [windowSize, defaultValue, getMaxCanvasSize])
+  }, [defaultValue, getLargeSpace, getMaxCanvasSize, windowSize])
 
   return (
     <section className="drawing-canvas__wrapper">
